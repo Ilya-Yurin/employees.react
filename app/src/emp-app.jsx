49cb8b7d9af5349da57544cfac6168f5552559f0
iyurin <ily2014yurin@gmail.com>
2017-07-15 02:04:33 +0300

    create layout of list item

--- app/src/emp-app.jsx
+++ app/src/emp-app.jsx
@@ -8,37 +8,35 @@ import * as Layots from './layouts/';
 class EmpApp extends Component {
   render() {
     return (
-      <div className='emp-app__wrapper'>
-        <Layots.EmpHeader>
-        </Layots.EmpHeader>
-        <div className='emp-app__content'>
-          <Layots.EmpSidebar>
-            <div className="emp-app__add--shadow">
-              <a href="/#add">
-                <i className="fa fa-plus-circle grey-fa-font"></i>
-              </a>
-            </div>
-            <MuiThemeProvider>
-              <List style={{padding: 0}}>
-                <Layots.EmpListItem>
-                  User #1
-                </Layots.EmpListItem>
-                <Layots.EmpListItem>
-                  User #2
-                </Layots.EmpListItem>
-                <Layots.EmpListItem>
-                  User #3
-                </Layots.EmpListItem>
-              </List>
-            </MuiThemeProvider>
-            <div className="emp-app__sidebar--footer">
-            </div>
-          </Layots.EmpSidebar>
-          <Layots.EmpContent>
-            Content text
-          </Layots.EmpContent>
+      <MuiThemeProvider>
+        <div className='emp-app__wrapper'>
+          <Layots.EmpHeader>
+          </Layots.EmpHeader>
+          <div className='emp-app__content'>
+            <Layots.EmpSidebar>
+              <div className="emp-app__add">
+                <a href="/#add">
+                  <i className="fa fa-plus-circle grey-fa-font"></i>
+                </a>
+              </div>
+              <Layots.EmpListItem>
+                User #1
+              </Layots.EmpListItem>
+              <Layots.EmpListItem>
+                User #2
+              </Layots.EmpListItem>
+              <Layots.EmpListItem>
+                User #3
+              </Layots.EmpListItem>
+              <div className="emp-app__sidebar--footer">
+              </div>
+            </Layots.EmpSidebar>
+            <Layots.EmpContent>
+              Content text
+            </Layots.EmpContent>
+          </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
--- app/src/layouts/components/list-item/emp-list-item.js
+++ app/src/layouts/components/list-item/emp-list-item.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import {ListItem} from 'material-ui/List';
+import Chip from 'material-ui/Chip';
 import Avatar from 'material-ui/Avatar';
 import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
 
 import './emp-list-item.styl';
 
-export const EmpListItem = props => (
-  <div className={props.className ? props.className : 'emp-list-item__shadow'}>
-    <ListItem
-      leftAvatar={<Avatar src="" />}
-      primaryText="Brunch this weekend?"
-      secondaryText={
-        <p>
-          <span style={{color: darkBlack}}>{props.children}</span> --
-          I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
-        </p>
-      }
-      secondaryTextLines={2}
-    />
-  </div>
-);
+export const EmpListItem = props => {
+
+  const progressStyle = () => {
+    return {backgroundColor: "#429321", top: "55%"};
+  };
+
+  return (
+    <div className={props.className ? props.className : 'emp-list-item'}>
+      <div className="emp-list-item__progress" style={progressStyle()}/>
+      <div className="emp-list-item__img">
+        <Avatar src="" size={50}/>
+      </div>
+      <div className="emp-list-item__data">
+        <div className="emp-list-item__name">
+          F.Name L.Name
+        </div>
+        <div className="emp-list-item__position">
+          Front-end developer
+        </div>
+        <div className="emp-list-item__skills">
+          <span className="emp-list-item__skills-tag">html</span>
+          <span className="emp-list-item__skills-tag">css</span>
+          <span className="emp-list-item__skills-tag">webpack</span>
+          <span className="emp-list-item__skills-tag">nodejs</span>
+          <span className="emp-list-item__skills-tag">angularjs</span>
+          <span className="emp-list-item__skills-tag">reactjs</span>
+          <span className="emp-list-item__skills-tag">git</span>
+          <span className="emp-list-item__skills-tag">rest</span>
+        </div>
+      </div>
+    </div>
+  );
+};
